383fe315462ccb20596125337abc2589c0981af4
Ferdous Ara Nargis <raniahmed@Ferdouss-MacBook-Pro.local>
2020-08-13 12:11:39 -0400

    PrintMatchedMonth class added

---
+++ src/PrintMatchedMonth.java
@@ -0,0 +1,18 @@
+public class PrintMatchedMonth {
+
+    public static void main(String[] args) {
+
+
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        for (String month : months) {
+            System.out.println(month);
+            boolean isDecember = month.equalsIgnoreCase("December");
+            if (isDecember == true) {
+                System.out.println("December is here");
+                break;
+            }
+
+        }
+    }
+
+}
