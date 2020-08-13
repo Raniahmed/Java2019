5f10ea3d9602731ee2373f92bd4483091dede3ac
Ferdous Ara Nargis <raniahmed@Ferdouss-MacBook-Pro.local>
2020-08-13 12:03:12 -0400

    Update even odd

--- src/HomeworkString.java
+++ src/HomeworkString.java
@@ -1,34 +1,55 @@
 import org.omg.CORBA.INTERNAL;
-
-public class HomeworkString {
-
-
-/*
-1)
-String myStatment = "I am a good programmer";
-Use string methods to do following tasks:
-	Extract "Programmer" string from myStatement.
-	Display the total number of words in the myStatement.
-	replace all the 'r' characters with 'f' characters.
-2)
-Store your name in a string variable.
-Calculate the length of your name, without using length() method of String class.
-3)
-String[] names = {"Mohammad" , "John", "Ferdous" , "Abul" , "Ratna" , "Mareeha" , "Ruslan" , "Himani"};
-Verify if "JOHN" is present at index 1.
-4)
-Array:
-Store first names of 5 of your friends/colleagues in an array, and display the following in console:
-    1. Length of that array
-    2. Print name written on 3rd position (hint: 2nd index)
-    3. Name of each friend/colleague
-    4. Does name on 2nd position, start with "A"
- */
-public static void main(String[] args) {
-
-    String myStatement = "I am a good programmer";
-
-    int len = myStatement.length();
-
-}
-}
+//
+//public class HomeworkString {
+//
+//
+///*
+//1)
+//String myStatment = "I am a good programmer";
+//Use string methods to do following tasks:
+//	Extract "Programmer" string from myStatement.
+//	Display the total number of words in the myStatement.
+//	replace all the 'r' characters with 'f' characters.
+//2)
+//Store your name in a string variable.
+//Calculate the length of your name, without using length() method of String class.
+//3)
+//String[] names = {"Mohammad" , "John", "Ferdous" , "Abul" , "Ratna" , "Mareeha" , "Ruslan" , "Himani"};
+//Verify if "JOHN" is present at index 1.
+//4)
+//Array:
+//Store first names of 5 of your friends/colleagues in an array, and display the following in console:
+//    1. Length of that array
+//    2. Print name written on 3rd position (hint: 2nd index)
+//    3. Name of each friend/colleague
+//    4. Does name on 2nd position, start with "A"
+// */
+//public static void main(String[] args) {
+//
+//    String myStatement = "I am a good programmer";
+//
+//    String prog = myStatement.substring(12);
+//    System.out.println(prog);
+//
+//    int len = myStatement.length();
+//
+//    System.out.println(len + "length");
+//    System.out.println("firstchar" + myStatement.charAt(0));
+//    System.out.println("lastchar" + myStatement.charAt(len-1));
+//
+//
+//    String x = "I am a good programmer";
+//    String y[] = x.split(" ");
+//    System.out.println(y.length);
+//
+//    String replacedString = myStatement.replace("r","f");
+//    System.out.println(replacedString);
+//
+//
+//    String firstName = "Ferdous";
+//    String lastName = "Nargis";
+//    String
+//
+//}
+//
+//}
